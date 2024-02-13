fc4fb11e7b089a61535cbf4fd6ac80c4236861ed
Stephen Swires <steve@swires.me>
2024-02-13 23:48:04 +0000

    Added super simple base widget that contains events for the pawn changing, or the player state becoming available.

---
+++ EasyHud/Source/EasyHud/Private/Widgets/EasyPlayerUserWidget.cpp
@@ -0,0 +1,64 @@
+﻿// Copyright Stephen Swires. All Rights Reserved.
+
+#include "Widgets/EasyPlayerUserWidget.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/PlayerState.h"
+
+void UEasyPlayerUserWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	RegisterPlayerDelegates();
+}
+
+void UEasyPlayerUserWidget::RegisterPlayerDelegates()
+{
+	APlayerController* PlayerController = GetOwningPlayer();
+
+	if (IsValid(PlayerController))
+	{
+		if (bAutoRegisterPawnChanged)
+		{
+			PlayerController->OnPossessedPawnChanged.AddDynamic(this, &UEasyPlayerUserWidget::OnPawnChangedPrivate);
+
+			if (APawn* CurrentPawn = PlayerController->GetPawn())
+			{
+				OnPawnChangedPrivate(nullptr, CurrentPawn);
+			}
+		}
+
+		if (bAutoRegisterPlayerStateChanged)
+		{
+			if (APlayerState* CurrentPlayerState = PlayerController->PlayerState)
+			{
+				OnPlayerStateChanged(CurrentPlayerState);
+			}
+			else
+			{
+				// Player state may not be available yet, so we'll listen for it
+				// But the bad news, Unreal has no delegate for this, so we'll have to poll until we get it
+				GetWorld()->GetTimerManager().SetTimer(
+					TimerHandle_CheckPlayerState,
+					this,
+					&UEasyPlayerUserWidget::CheckForPlayerState,
+					0.5f,
+					true);
+			}
+		}
+	}
+}
+
+void UEasyPlayerUserWidget::OnPawnChangedPrivate(APawn* InOldPawn, APawn* InNewPawn)
+{
+	OnPawnChanged(InOldPawn, InNewPawn);
+}
+
+void UEasyPlayerUserWidget::CheckForPlayerState()
+{	
+	APlayerController* PlayerController = GetOwningPlayer();
+
+	if (IsValid(PlayerController) && IsValid(PlayerController->PlayerState))
+	{
+		GetWorld()->GetTimerManager().ClearTimer(TimerHandle_CheckPlayerState);
+		OnPlayerStateChanged(PlayerController->PlayerState);
+	}
+}
---
+++ EasyHud/Source/EasyHud/Public/Widgets/EasyPlayerUserWidget.h
@@ -0,0 +1,58 @@
+﻿// Copyright Stephen Swires. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EasyPlayerUserWidget.generated.h"
+
+class APawn;
+class APlayerState;
+
+/**
+ * Base widget that fires events when the pawn changes and when the player state is available
+ */
+UCLASS(Abstract)
+class EASYHUD_API UEasyPlayerUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeConstruct() override;
+
+	// Event called when the owning player's pawn changes.
+	// The old pawn may be null if the player had no pawn, which will be the case the first time this event is called.
+	UFUNCTION(BlueprintNativeEvent, Category = "Player")
+	void OnPawnChanged(APawn* InOldPawn, APawn* InNewPawn);
+	virtual void OnPawnChanged_Implementation(APawn* InOldPawn, APawn* InNewPawn) {}
+
+	// Event called when player state is available
+	UFUNCTION(BlueprintNativeEvent, Category = "Player")
+	void OnPlayerStateChanged(APlayerState* InPlayerState);
+	virtual void OnPlayerStateChanged_Implementation(APlayerState* InPlayerState) {}
+
+protected:
+
+	// Auto register delegate to the owning player's pawn changed event
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	bool bAutoRegisterPawnChanged = false;
+
+	// Auto register delegate to the owning player's player state changed event
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	bool bAutoRegisterPlayerStateChanged = false;
+
+private:
+	
+	void RegisterPlayerDelegates();
+
+	// Dynamic delegate callback when the owner player's pawn changes
+	UFUNCTION()
+	void OnPawnChangedPrivate(APawn* InOldPawn, APawn* InNewPawn);
+
+	void CheckForPlayerState();
+	
+	UPROPERTY()
+	FTimerHandle TimerHandle_CheckPlayerState;
+	
+};
